8ea8ab3f8ce40006fe954511e069238526fbc435
jdeksnys <jonasdeksnys@gamil.com>
2024-04-27 20:13:20 +0300

    tcr: tests pass

--- tdd-mooc-small-steps-main/src/prices.mjs
+++ tdd-mooc-small-steps-main/src/prices.mjs
@@ -34,6 +34,12 @@ function createApp(database) {
     }
   }
 
+  function parseDateTemp(dateString) {
+    if (dateString) {
+      return new Date(dateString);
+    }
+  }
+
   function calculateCost(age, type, date, baseCost) {
     if (type === "night") {
       return calculateCostForNightTicket(age, baseCost);

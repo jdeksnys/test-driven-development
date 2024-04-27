d24a7d56a029d0080a263c1aecedc3f4a0829f64
jdeksnys <jonasdeksnys@gamil.com>
2024-04-27 19:40:33 +0300

    tcr: tests pass

--- tdd-mooc-small-steps-main/src/prices.mjs
+++ tdd-mooc-small-steps-main/src/prices.mjs
@@ -77,8 +77,7 @@ function createApp(database) {
   }
 
   function isMonday(date) {
-    let tempDate = null;
-    tempDate = date.toTemporalInstant().toZonedDateTimeISO("UTC").toPlainDate();
+    let tempDate = date.toTemporalInstant().toZonedDateTimeISO("UTC").toPlainDate();
     if(date!=undefined && date!=null){
     }
     return date.getDay() === 1;
